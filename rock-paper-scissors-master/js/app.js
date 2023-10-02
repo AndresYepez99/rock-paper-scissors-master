0b6517dbf19e8fc3afbd9b7c40e86a69f321ea8d
Andres Yepez <andresyepezmn@gmail.com>
2023-10-01 23:30:13 -0500

    feat: Crear funciones en app.js y seleccionar elementos del DOM en elements.js

--- rock-paper-scissors-master/js/app.js
+++ rock-paper-scissors-master/js/app.js
@@ -1,8 +1,70 @@
 'use strict';
 /* DOM elements */
+import {
+  $blockButtons,
+  $blockModalRulesContainer,
+  $btnCloseModalRules,
+  $btnRules,
+  $pointCounter,
+} from './elements.js';
 
 /* Global variables */
+let points = 0;
 
 /* Functions */
+/* Generate machine option */
+const machinePlays = function () {
+  let option = Math.trunc(Math.random() * 3 + 1);
+  switch (option) {
+    case 1:
+      option = 'rock';
+      break;
+    case 2:
+      option = 'paper';
+    case 3:
+      option = 'scissors';
+      break;
+  }
+  return option;
+};
+
+/* play a round */
+const playRun = function (optionPlayer, optionMachine) {
+  /* console.log(optionPlayer, optionMachine); */
+
+  if (optionPlayer === optionMachine) {
+    console.log(`it's a mess`);
+  } else if (
+    (optionPlayer === 'rock' && optionMachine === 'scissors') ||
+    (optionPlayer === 'paper' && optionMachine === 'rock') ||
+    (optionPlayer === 'scissors' && optionMachine === 'paper')
+  ) {
+    console.log(`you win!`);
+    points++;
+  } else {
+    console.log(`you lose!`);
+  }
+};
 
 /* Event Listeners */
+
+/* Game buttons events */
+$blockButtons.addEventListener('click', function (event) {
+  /* capture player option */
+  const optionPlayer = event.target.classList[1];
+  playRun(optionPlayer, machinePlays());
+  
+  /* update player points */
+  $pointCounter.textContent = points;
+});
+
+/* Modal rules events */
+/* Open */
+$btnRules.addEventListener('click', function () {
+  $blockModalRulesContainer.style.display = 'grid';
+});
+
+/* Close */
+$btnCloseModalRules.addEventListener('click', function () {
+  $blockModalRulesContainer.style.display = 'none';
+});
---
+++ rock-paper-scissors-master/js/elements.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const $pointCounter = document.querySelector('.points');
+const $blockButtons = document.querySelector('.buttons');
+const $blockModalRulesContainer = document.querySelector('.modal-rules-container');
+const $btnRules = document.querySelector('.btn-rules');
+const $btnCloseModalRules = document.querySelector('.icon-close');
+
+export {
+    $blockButtons,
+    $blockModalRulesContainer,
+    $btnCloseModalRules,
+    $btnRules,
+    $pointCounter
+};
